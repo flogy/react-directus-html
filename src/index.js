4541b03afb95a77105c7105b27f10b81232c5fd2
Florian Gyger <gygy1992@hotmail.com>
2018-03-21 21:02:59 +0100

    removed CSS styling

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import styles from './styles.css'
 
 export default class ExampleComponent extends Component {
   static propTypes = {
